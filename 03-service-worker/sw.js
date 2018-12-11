d8d17630b33066db4d388f5202c4659dca60ed9b
Erik Villegas <erdvillegas@gmail.com>
2018-12-11 00:01:38 -0600

    Evento Install

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,19 @@
 
 // Ciclo de vida del SW
 
+//Cuando se instala el SW
+self.addEventListener('install', event=>{
+    // Descargamos assets
+    // Creamos un cache
+    console.log("SW: Instalando WS");
+    
+    self.skipWaiting();
+});
+
+//Cuando el SW toma el control de la aplicación
+
+self.addEventListener('activate', event=>{
+    // Borrar cache viejo
+
+    console.log("SW: Activo y listo para controlar la App");
+});
