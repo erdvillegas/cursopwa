90deb371c75e5c3de3ffc1f6e53bb39c9093cab1
Erik Villegas <erdvillegas@gmail.com>
2018-12-11 22:45:22 -0600

    Seccion 3 Completada

--- 03-service-worker/js/app.js
+++ 03-service-worker/js/app.js
@@ -1,6 +1,21 @@
 
 
 // Detectar si podemos usar Service Workers
-if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+            // setTimeout(() => {
+            //     reg.sync.register('posteo-gatitos');
+            //     console.log("Se enviaron fotos al server");
+            // }, 300);
+            Notification.requestPermission().then(result => {
+                console.log(result);
+                reg.showNotification("HOla Mundo!");
+            });
+        });
 }
+
+
+// fetch('https://reqres.in/api/users')
+// .then( resp=> resp.text())
+// .then( console.log);
--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,19 +1,47 @@
-
 // Ciclo de vida del SW
 
 //Cuando se instala el SW
-self.addEventListener('install', event=>{
-    // Descargamos assets
-    // Creamos un cache
-    console.log("SW: Instalando WS");
-    
-    self.skipWaiting();
+self.addEventListener("install", event => {
+  // Descargamos assets
+  // Creamos un cache
+  console.log("SW: Instalando WS");
+
+  const instalacion = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("SW: Instalaciones terminadas");
+      self.skipWaiting();
+      resolve();
+    }, 1);
+  });
+
+  event.waitUntil(instalacion);
 });
 
 //Cuando el SW toma el control de la aplicación
+self.addEventListener("activate", event => {
+  // Borrar cache viejo
+  console.log("SW: Activo y listo para controlar la App");
+});
 
-self.addEventListener('activate', event=>{
-    // Borrar cache viejo
+// FETCH: Manejo de peticiones HTTP
+self.addEventListener("fetch", event => {
+  // Aplicar estrategias del cache
+  // console.log("SW:", event.request.url);
+  // if( event.request.url.includes('https://reqres.in/')){
+  //     const resp = new Response(`{ok: false, mensaje: 'xD'}`);
+  //     event.respondWith( resp );
+  // }
+});
 
-    console.log("SW: Activo y listo para controlar la App");
+// SYNC: Recuperamos la conexión a internet
+self.addEventListener("sync", event => {
+  console.log("Tenemos conexión!");
+  console.log(event);
+  console.log(event.tag);
+});
+
+// PUSH: Maneja las push notificaciones
+self.addEventListener('push',event=>{
+    console.log('Notificacion recibida');
+    console.log(event);
 });
