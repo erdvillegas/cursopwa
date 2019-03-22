eb827cf3408dfa8aeeaf4ccc494c649af5475a9f
Erik Villegas <erdvillegas@gmail.com>
2019-03-22 00:43:17 -0600

    Configuracion de servicios

--- 11-twittor-con-push/server/routes.js
+++ 11-twittor-con-push/server/routes.js
@@ -5,41 +5,55 @@ var router = express.Router();
 
 const mensajes = [
 
-  {
-    _id: 'XXX',
-    user: 'spiderman',
-    mensaje: 'Hola Mundo'
-  }
+    {
+        _id: 'XXX',
+        user: 'spiderman',
+        mensaje: 'Hola Mundo'
+    }
 
 ];
 
 
 // Get mensajes
-router.get('/', function (req, res) {
-  // res.json('Obteniendo mensajes');
-  res.json( mensajes );
+router.get('/', function(req, res) {
+    // res.json('Obteniendo mensajes');
+    res.json(mensajes);
 });
 
 
 // Post mensaje
-router.post('/', function (req, res) {
-  
-  const mensaje = {
-    mensaje: req.body.mensaje,
-    user: req.body.user
-  };
+router.post('/', function(req, res) {
 
-  mensajes.push( mensaje );
+    const mensaje = {
+        mensaje: req.body.mensaje,
+        user: req.body.user
+    };
 
-  console.log(mensajes);
+    mensajes.push(mensaje);
+
+    console.log(mensajes);
 
 
-  res.json({
-    ok: true,
-    mensaje
-  });
+    res.json({
+        ok: true,
+        mensaje
+    });
 });
 
+// Almacena la suscripcion del cliente
+router.post('/suscribe', (req, res) => {
+    res.json('suscribe');
+});
 
+// Almacena la suscripcion y envia el key publico al cliente
+router.get('/key', (req, res) => {
+    res.json('key publico');
+});
+
+// Envia una notificacion PUSH a las personas
+// Esto es solo para el BackEnd
+router.post('/push', (req, res) => {
+    res.json('key publico');
+});
 
 module.exports = router;
