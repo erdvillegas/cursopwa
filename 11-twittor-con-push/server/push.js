1bbd3a24a55f4158a733bccc7911eec8963fc73f
Erik Villegas <erdvillegas@gmail.com>
2019-07-19 00:44:39 -0500

    Agregando archivos tema 12

--- 11-twittor-con-push/server/push.js
+++ 11-twittor-con-push/server/push.js
@@ -9,7 +9,9 @@ webpush.setVapidDetails(
     vapid.privateKey
 );
 
-const suscripciones = require('./subs-db.json');
+let suscripciones = require('./subs-db.json');
+
+const notificacionesEnviadas = [];
 
 module.exports.getKey = () => {
     return urlsafeBase64.decode(vapid.publicKey);
@@ -22,6 +24,21 @@ module.exports.addSubscription = (suscripcion) => {
 
 module.exports.sendPush = (post) => {
     suscripciones.forEach((suscripcion, i) => {
-        webpush.sendNotification(suscripcion, JSON.stringify(post));
+
+        const pushProm = webpush.sendNotification(suscripcion, JSON.stringify(post))
+            .then(console.log('Notificacion enviada'))
+            .catch(err => {
+                if (err.statusCode === 410) { //410 -> GONE, ya no existe
+                    suscripciones[i].borrar = true;
+                }
+            });
+
+        notificacionesEnviadas.push(pushProm);
+    });
+
+    Promise.all(notificacionesEnviadas).then(() => {
+        suscripciones = suscripciones.filter(subs => !subs.borrar);
+
+        fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones));
     });
 };
